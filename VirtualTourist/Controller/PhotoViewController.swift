ff4a1fd5d9a00e5a6ded089fc7285056ac4ba2c0
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-03 04:31:41 -0500

    feat: Remove photos

--- VirtualTourist/Controller/PhotoViewController.swift
+++ VirtualTourist/Controller/PhotoViewController.swift
@@ -18,12 +18,51 @@ class PhotoViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    var dictionarySelectedIndexPath: [IndexPath: Bool] = [:]
     
     var data = [PhotoItemResponse]()
     
+    lazy var selectBarButton: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(title: "Select", style: .plain, target: self, action: #selector(didSelectButtonClicked(_:)))
+        return barButtonItem
+      }()
+
+      lazy var deleteBarButton: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(didDeleteButtonClicked(_:)))
+        return barButtonItem
+      }()
+    
+    enum Mode {
+        case view
+        case select
+    }
+    
+    var mode: Mode = .view {
+        didSet {
+            switch mode {
+            case .view:
+                for (key, value) in dictionarySelectedIndexPath {
+                    if value {
+                        collectionView.deselectItem(at: key, animated: true)
+                    }
+                }
+            
+                dictionarySelectedIndexPath.removeAll()
+            
+                selectBarButton.title = "Select"
+                navigationItem.leftBarButtonItem = nil
+                collectionView.allowsMultipleSelection = false
+            case .select:
+                selectBarButton.title = "Cancel"
+                navigationItem.leftBarButtonItem = deleteBarButton
+                collectionView.allowsMultipleSelection = true
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        configureFlowLayout()
+        navigationItem.rightBarButtonItem = selectBarButton
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,9 +78,24 @@ class PhotoViewController: UIViewController {
         }
     }
     
-    @IBAction func deletePhoto(_ sender: UIBarButtonItem) {
+    @objc func didSelectButtonClicked(_ sender: UIBarButtonItem) {
+        mode = mode == .view ? .select : .view
     }
+      
+    @objc func didDeleteButtonClicked(_ sender: UIBarButtonItem) {
+        var deleteNeededIndexPaths: [IndexPath] = []
+        for (key, value) in dictionarySelectedIndexPath {
+            if value {
+                deleteNeededIndexPaths.append(key)
+            }
+        }
+        
+        for i in deleteNeededIndexPaths.sorted(by: { $0.item > $1.item }) {
+            data.remove(at: i.item)
+        }
+        
+        collectionView.deleteItems(at: deleteNeededIndexPaths)
+        dictionarySelectedIndexPath.removeAll()
+      }
     
-    @IBAction func back(_ sender: UIBarButtonItem) {
-    }
 }
--- VirtualTourist/Data/Api/EndPoint.swift
+++ VirtualTourist/Data/Api/EndPoint.swift
@@ -11,12 +11,12 @@ enum EndPoint {
     static let apiKey = "c16d6433413442d5dc85a8928d901540"
     static let urlBase = "https://www.flickr.com/services/rest/"
     
-    case photoSearch(Double, Double)
+    case photoSearch(Double, Double, Int)
     case image(String)
     
     var value: String {
         switch self {
-        case .photoSearch(let latitude, let longitude):
+        case .photoSearch(let latitude, let longitude, let page):
             return EndPoint.urlBase
                 + "?method=flickr.photos.search"
                 + "&api_key=\(EndPoint.apiKey)" +
@@ -24,7 +24,7 @@ enum EndPoint {
                 "&lon=\(longitude)" +
                 "&radius=20" +
                 "&per_page=20" +
-                "&page=1" +
+                "&page=\(page)" +
             "&format=json&nojsoncallback=1&extras=url_m"
         case .image(let url):
             return url
