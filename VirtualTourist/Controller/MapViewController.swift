46028234d34857be604610314eaec57f2581ef1c
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-02 20:59:28 -0500

    feat: Add pin entity

--- VirtualTourist/Controller/MapViewController.swift
+++ VirtualTourist/Controller/MapViewController.swift
@@ -1,14 +1,17 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  VirtualTourist
 //
 //  Created by Ricardo Bravo on 2/07/21.
-//
+//s
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
