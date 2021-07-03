d7b12cf16dac4bdf17fda282b2b7999aed4732df
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-03 01:11:54 -0500

    feat: Add photo view cell

--- VirtualTourist/Controller/PhotoViewController.swift
+++ VirtualTourist/Controller/PhotoViewController.swift
@@ -7,14 +7,25 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class PhotoViewController: UIViewController {
     
+    @IBOutlet weak var navigation: UINavigationItem!
     var pin: CustomPointAnnotation!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var newCollectionButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Pin \(pin)")
     }
     
+    @IBAction func deletePhoto(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func back(_ sender: UIBarButtonItem) {
+    }
 }
