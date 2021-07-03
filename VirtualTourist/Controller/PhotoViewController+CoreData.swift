4ea5b116f4587035f8ae9fed1d8cea4a9716ed7a
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-03 18:00:42 -0500

    feat: Add ns fetched results to collection view

---
+++ VirtualTourist/Controller/PhotoViewController+CoreData.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoViewController+CoreData.swift
+//  VirtualTourist
+//
+//  Created by Ricardo Bravo on 3/07/21.
+//
+
+import Foundation
+import CoreData
+
+extension PhotoViewController: NSFetchedResultsControllerDelegate {
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any,
+                    at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath:  IndexPath?) {
+        switch type {
+        case .insert:
+            self.collectionView.insertItems(at: [newIndexPath!])
+        case .delete:
+            self.collectionView.deleteItems(at: [indexPath!])
+        case .update:
+            self.collectionView.reloadItems(at: [indexPath!])
+        default:
+            break
+        }
+    }
+    
+    
+}
